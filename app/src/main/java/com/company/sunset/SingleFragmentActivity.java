aeb80c712f2561219caa99181258875f2cf11758
Hector Leyva Villanueva <hectorleyvavillanueva@Hectors-MacBook-Pro.local>
2017-01-28 14:24:16 -0800

    First commit

---
+++ app/src/main/java/com/company/sunset/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.company.sunset;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hectorleyvavillanueva on 1/24/17.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    public abstract Fragment createFragment();
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_container);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.container, fragment)
+                    .commit();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/company/sunset/SunsetActivity.java
@@ -0,0 +1,11 @@
+package com.company.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
